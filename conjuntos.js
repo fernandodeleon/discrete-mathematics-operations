f57a97a655d6bbcf7135d8329bf5a64329e695d8
Jorge Guerra <jorgeguerrask8@gmail.com>
2018-10-16 10:08:34 -0600

    ADD
    Funcionalidad para la interfaz lista.

---
+++ conjuntos.js
@@ -0,0 +1,20 @@
+$( document ).ready( function () {
+  $( '#mySelect' ).change( function () {
+
+    if (document.getElementById("inner").hasChildNodes()) {
+      $( "#inner" ).empty();
+    }
+
+
+
+    let html = null;
+    let value = $( this ).val();
+    for ( var x = 0; x < value; x++ ) {
+      html = `<div class="input-field">
+          <input id="conjunto${x + 1}" type="text" class="validate">
+          <label for="conjunto${x + 1}">Conjunto ${x + 1}</label>
+        </div>`
+      $( "#inner" ).append( html );
+    }
+  });
+});
